d340d607e86acecc010ac0dc2a12572eb1303e3c
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-03-03 20:25:39 +0200

    yey

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import {Link} from 'gatsby'
+
+// markup
+const AboutPage = () => {
+  return (
+    <main>
+        <Link to="/">Home page</Link>
+        <p>About</p>
+    </main>
+  )
+}
+
+export default AboutPage
